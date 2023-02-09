094b5ffbc03b07acb4a39f46a82f71e6770a2756
Na Young Jung <nang518@icloud.com>
2023-02-09 17:43:47 +0900

    [feat]: 카테고리별 구분 기능

--- eatJNU/View/PopularItemRowView.swift
+++ eatJNU/View/PopularItemRowView.swift
@@ -156,6 +156,48 @@ struct PopularItemRowView: View {
                         }
                     }
                 }
+                else if (selectedFilterNum == 0) {
+                    HStack(spacing: 2) {
+                        URLImage(url){ image in
+                            image
+                                .resizable()
+                                .frame(width: 88, height: 88)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 24)
+                                        .stroke(Color.black, lineWidth: 2)
+                                )
+                                .cornerRadius(24)
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 2) {
+                            Text(post.name)
+                                .font(.system(size: 16, weight: .bold))
+                            
+                            
+                            HStack {
+                                Image("like2")
+                                    .frame(width: 16, height: 16)
+                                
+                                Text("\(post.likeCount)")
+                                    .font(.custom("BMDOHYEON-OTF", size: 14))
+                                
+                                
+                                Text("리뷰 \(post.reviewCount)")
+                                    .font(.system(size: 14))
+                                    .foregroundColor(.gray)
+                                
+                                
+                                
+                            }
+                            
+                            //                            Text(post.tags)
+                            //                                .foregroundColor(Color(.systemGray2))
+                            //                                .font(.system(size: 12))
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                    }
+                }
                 else {
                     Text("ERROR")
                 }
